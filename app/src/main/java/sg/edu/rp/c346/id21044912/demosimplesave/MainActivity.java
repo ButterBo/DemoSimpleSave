42eae47abe57f0d54063870c91967c477b0891c2
ButtorBo <21044912@myrp.edu.sg>
2022-07-06 10:17:57 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21044912/demosimplesave/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.id21044912.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Step 1: Obtain the SharedPreferences instance
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        //Step 2: Create a SharedPreferences Editor by calling edit()
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        //Step 3: Set a key-value pair in the editor
+        prefEdit.putString("greetings","Hello!");
+        //Step 4 call commit() to save changes made to SharedPreferences
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String msg = "No greetings!";
+        Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
